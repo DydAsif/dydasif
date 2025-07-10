3cf05e2c3a2c8e1446d8c65b43df1f371990bca7
ASIF <dydfreelancer@gmail.com>
2025-07-10 02:47:48 +0000

    🎥 Hero Animation Vision: Powerful + Cinematic Flow (Refined): Fade in y

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React from 'react';
@@ -10,7 +11,6 @@ const handleScrollTo = (e: React.MouseEvent<HTMLAnchorElement>, selector: string
     element.scrollIntoView({ behavior: 'smooth' });
   }
 };
-
 
 export function Hero() {
   return (
