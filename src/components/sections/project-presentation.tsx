2b9b1bba633181294816fd823acdde08f8a43f0b
ASIF <dydfreelancer@gmail.com>
2025-07-12 19:54:31 +0000

    solve

--- src/components/sections/project-presentation.tsx
+++ src/components/sections/project-presentation.tsx
@@ -26,7 +26,7 @@ const projects: Project[] = [
         trigger: 'Result',
         value: 'result',
         content: '100% purchase tracking & EMQ 8.7',
-        imageUrl: 'https://i.ibb.co/QFfvqD7/Result.png',
+        imageUrl: '/Result.png',
         imageHint: 'dashboard graph success',
       }
     ]
