6da13f769e8df9b16e2e05f52180dd570245e35b
ASIF <dydfreelancer@gmail.com>
2026-03-03 18:58:57 +0000

    File changes

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+// It is safe to expose this configuration to the client
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1F7bPeBszwoB5fGJJEsOaiKLg2mDq3mA",
+  authDomain: "studio-9142636133-a7e4e.firebaseapp.com",
+  projectId: "studio-9142636133-a7e4e",
+  messagingSenderId: "889898258405",
+  appId: "1:889898258405:web:fe1a3d2989dd89d86ea90c",
+  measurementId: "",
+};
