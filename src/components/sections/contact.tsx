12f8e56dcefbf3435e230fbda1adb37776da23bc
ASIF <dydfreelancer@gmail.com>
2026-03-12 19:25:09 +0000

    https://x.com/AsifAshfakur use this for twitter and can you change logo

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -10,7 +10,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useToast } from '@/hooks/use-toast';
 import { submitContactForm } from '@/app/actions';
-import { Loader2, Mail, Linkedin, Facebook, Instagram, Twitter } from 'lucide-react';
+import { Loader2, Mail, Linkedin, Facebook, Instagram } from 'lucide-react';
 import { Card } from '../ui/card';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
@@ -49,6 +49,12 @@ const WhatsAppIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 24 24" fill="currentColor"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.919 6.066l-1.425 4.185 4.352-1.141z" /></svg>
 );
 
+const XIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+    </svg>
+);
+
 
 const freelancePlatforms = [
   { label: 'Fiverr', href: 'https://www.fiverr.com/a_r_asif', icon: <FiverrIcon /> },
@@ -60,7 +66,7 @@ const socialMediaLinks = [
   { label: 'LinkedIn', href: 'https://www.linkedin.com/in/ashfakur-rahman-asif-42231b369/', icon: <Linkedin className="h-8 w-8" /> },
   { label: 'Instagram', href: 'https://www.instagram.com/it_is_me_asif?igsh=MWFiODhlcnZkY2JhNw==', icon: <Instagram className="h-8 w-8" /> },
   { label: 'Facebook', href: 'https://www.facebook.com/share/12KAR3Dqt58/', icon: <Facebook className="h-8 w-8" /> },
-  { label: 'Twitter', href: '#', icon: <Twitter className="h-8 w-8" /> },
+  { label: 'Twitter', href: 'https://x.com/AsifAshfakur', icon: <XIcon /> },
   { label: 'WhatsApp', href: 'https://wa.me/8801636335194', icon: <WhatsAppIcon /> },
 ];
 
